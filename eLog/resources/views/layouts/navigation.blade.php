6bffe73e2b1556ecc47d0c5500a5a66a370ebbed
lcasort <laucasort.notes@gmail.com>
2023-11-20 20:25:20 +0100

    Navbar overlay done

--- eLog/resources/views/layouts/navigation.blade.php
+++ eLog/resources/views/layouts/navigation.blade.php
@@ -1,100 +1,75 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <img class="h-20 fill-current text-gray-500" src="{{asset('/img/logo/logo_banner_light.png')}}" alt="eLog" />
+<div class="container-fluid align-self-start">
+    <div class="row flex-nowrap">
+        <div class="col-auto px-sm-2 px-0">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <!-- Logo and collapse button -->
+                <div class="d-flex flex-row align-items-center py-3">
+                    <a href="{{ route("dashboard") }}" class="d-flex align-items-center mb-md-0 me-md-auto text-white text-decoration-none">
+                        <img class="nav-bar-logo"
+                            src="{{asset('/img/logo/logo_light.png')}}"
+                            alt="eLog" />
+                    </a>
+                    <a href="#" class="collapse-button">
+                        <i class="fa-solid fa-bars"></i>
                     </a>
                 </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
+                <!-- Pages -->
+                <div class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start py-3" id="menu">
+                    <div class="nav-item">
+                        <a href="{{ route("dashboard") }}" class="nav-link align-middle px-0 d-flex flex-row">
+                            <div class="text-center icon pr-3">
+                                <i class="fa-regular fa-window-restore"></i>
                             </div>
-                        </button>
-                    </x-slot>
+                            <div><span class="ms-1 d-none d-sm-inline">Dashboard</span></div>
+                        </a>
+                    </div>
+                    <div class="nav-item">
+                        <!-- TODO: Link this to the mood individual view page. -->
+                        <a href="#" class="nav-link align-middle px-0 d-flex flex-row">
+                            <div class="text-center icon pr-3">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                            <div><span class="ms-1 d-none d-sm-inline">Mood</span></div>
+                        </a>
+                    </div>
+                    <div class="nav-item">
+                        <!-- TODO: Link this to the habits individual view page. -->
+                        <a href="#" class="nav-link align-middle px-0 d-flex flex-row">
+                            <div class="text-center icon pr-3">
+                                <i class="fa-solid fa-list-check"></i>
+                            </div>
+                            <div><span class="ms-1 d-none d-sm-inline">Habits</span></div>
+                        </a>
+                    </div>
+                    <div class="nav-item">
+                        <!-- TODO: Link this to the journal individual view page. -->
+                        <a href="#" class="nav-link align-middle px-0 d-flex flex-row">
+                            <div class="text-center icon pr-3">
+                                <i class="fa-regular fa-pen-to-square"></i>
+                            </div>
+                            <div><span class="ms-1 d-none d-sm-inline">Journal</span></div>
+                        </a>
+                    </div>
+                </div>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
+                <!-- User's profile and logout -->
+                <div class="btn-group d-flex flex-row align-items-center justify-content-around w-100 pb-3 text-muted">
+                    <div class="user-button pr-3 flex-grow-1">
+                        <a class="btn btn-outline-light w-100" href="{{ route('profile.edit') }}">
+                            {{ Auth::user()->name }}
+                        </a>
+                    </div>
+                    <div>
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
+                            <a class="logout-button" href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); this.closest('form').submit();">
+                                <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                            </a>
                         </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
-    </div>
-</nav>
+</div>
