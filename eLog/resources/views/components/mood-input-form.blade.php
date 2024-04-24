638818bb03813190a3fca1cda0c278a6cffaff46
laucasort <laucasort.notes@gmail.com>
2024-04-24 19:31:17 +0200

    Added comments to mood-input-form component

--- eLog/resources/views/components/mood-input-form.blade.php
+++ eLog/resources/views/components/mood-input-form.blade.php
@@ -1,6 +1,14 @@
 <div class="col">
-    <!-- Show today's mood -->
+    <!--
+        \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+                                   SHOW TODAY'S MOOD
+        ////////////////////////////////////////////////////////////////////////
+    -->
     <div class="row m-0 justify-content-center">
+        <!--
+            If there's already a mood selected for today we show it, else we
+            show a placeholder for it.
+        -->
         @if ($moodToday)
         <div class="mood-today-set">
             <img class="mood-today-pic"
@@ -13,6 +21,15 @@
         @endif
     </div>
 
+    <!--
+        \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+                            WE SHOW THE MOOD SELECTION FORM
+        ////////////////////////////////////////////////////////////////////////
+    -->
+    <!--
+        If the user has already selected the mood for today, we update it.
+        Else, we save it as a new one.
+    -->
     <form
         action="{{ $moodToday ? route('mood.update') : route('mood.save') }}"
         method="POST">
@@ -26,6 +43,7 @@
         <fieldset class="mt-6">
             <legend class="text-center mb-3">Moods</legend>
             <div class="d-flex flex-wrap justify-content-center">
+                <!-- We show the different mood options -->
                 @foreach ($moodOptions as $option)
                 <input type="radio" class="btn-check" name="mood" id="{{$option->name}}" autocomplete="off" value="{{$option->name}}" required>
                 <label class="btn btn-light p-0 selected-pic mx-2" for="{{$option->name}}">
@@ -35,6 +53,7 @@
             </div>
         </fieldset>
 
+        <!-- Button that saves/updates the mood -->
         <div class="text-center mt-3">
             <input type="submit" class="btn submit-button" value="Submit">
         </div>
