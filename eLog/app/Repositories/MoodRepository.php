47ce723adc834cb896256e09ca8263cdf1f3148f
laucasort <laucasort.notes@gmail.com>
2024-04-24 18:42:50 +0200

    Updated comments on MoodRepository

--- eLog/app/Repositories/MoodRepository.php
+++ eLog/app/Repositories/MoodRepository.php
@@ -46,9 +46,13 @@ class MoodRepository
      */
     public function getTodaysMood(Collection $moods): Mood|null
     {
+        // We get the current date
         $today = $this->dateService->getCurrentDate();
+        // We get the mood of today from the collection
         $moodToday = $moods->where('date', 'like', $today)->first();
 
+        // If there's a mood for today, we return it.
+        // Else, we return null.
         return $moodToday ? $moodToday->mood : null;
     }
     
@@ -60,9 +64,11 @@ class MoodRepository
      */
     public function getUserMoodsWithParsedDate(User $user): Collection
     {
+        // We get all the user moods
         $moods = $user->userMoods()->with(['mood'])->get()->map(
             function ($mood)
             {
+                // We format the dates
                 $mood->date = Carbon::parse($mood->created_at)->format('Y-m-d');
                 return $mood;
             }
@@ -79,14 +85,19 @@ class MoodRepository
      */
     public function mergeMoodsAndDays(Collection $moods): array
     {
+        // We get the start and end of the month
         $endOfMonth = Carbon::now()->endOfMonth();
         $startOfMonth = Carbon::now()->startOfMonth();
 
+        // We iterate through the days of the month
         $moodsByDate = [];
         for ($i = 0; $i < $endOfMonth->day; $i++)
         {
+            // We get the day formated
             $date = Carbon::parse($startOfMonth)->addDays($i)->format('Y-m-d');
+            // We get the mood for said date
             $mood = $moods->where('date', 'like', $date)->first();
+            // We add it to the array
             $moodsByDate[$date] = $mood;
         }
 
@@ -102,13 +113,17 @@ class MoodRepository
      */
     public function saveUserMood(User $user, string $mood): void
     {
+        // We get the mood
         $mood = Mood::where('name', $mood)->firstOrFail();
 
+        // We create an array with the necessary data to save the user's mood
+        // for today
         $data = [
             'user_id' => $user->id,
             'mood_id' => $mood->id,
         ];
         
+        // We save the user's mood with the current date
         UserMood::create($data);
     }
     
@@ -121,8 +136,10 @@ class MoodRepository
      */
     public function updateUserMood(User $user, string $mood): void
     {
+        // We get the mood
         $mood = Mood::where('name', $mood)->firstOrFail();
 
+        // We get the user's mood for today and update it
         $todaysMood = $this->getTodaysUserMood($user);
         $todaysMood->mood_id = $mood->id;
         $todaysMood->save();
@@ -143,8 +160,10 @@ class MoodRepository
      */
     private function getTodaysUserMood(User $user): UserMood
     {
+        // We get the current date
         $today = Carbon::now()->toDateString();
 
+        // We return the user's mood today
         return $user->userMoods()
             ->where('updated_at', 'like', $today.'%')
             ->firstOrFail();
